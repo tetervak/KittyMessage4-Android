a4839f5d2ee7174ad6b3ed12b174c1c126490161
Alex Tetervak <tetervak@sheridancollege.ca>
2020-10-02 09:22:26 -0400

    used the fragment back stack

--- app/src/main/java/ca/tetervak/kittymessage4/MainActivity.kt
+++ app/src/main/java/ca/tetervak/kittymessage4/MainActivity.kt
@@ -2,12 +2,17 @@ package ca.tetervak.kittymessage4
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import ca.tetervak.kittymessage4.model.Envelope
 import ca.tetervak.kittymessage4.ui.InputFragment
 import ca.tetervak.kittymessage4.ui.OutputFragment
 
 class MainActivity : AppCompatActivity(),
     InputFragment.InputListener, OutputFragment.OutputListener {
+
+    companion object{
+        const val INPUT_TO_OUTPUT = "input_to_output"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +28,12 @@ class MainActivity : AppCompatActivity(),
     override fun showOutput(envelope: Envelope) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container, OutputFragment.newInstance(envelope))
+            .addToBackStack(INPUT_TO_OUTPUT)
             .commit()
     }
 
     override fun showInput(){
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, InputFragment())
-            .commit()
+        supportFragmentManager.popBackStack(
+            INPUT_TO_OUTPUT, FragmentManager.POP_BACK_STACK_INCLUSIVE)
     }
 }
