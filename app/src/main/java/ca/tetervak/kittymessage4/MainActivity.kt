4436d9f758eb00eec1d703852ad40f1dd271e6fd
Alex Tetervak <tetervak@sheridancollege.ca>
2020-10-02 10:07:45 -0400

    used NavComponent

--- app/src/main/java/ca/tetervak/kittymessage4/MainActivity.kt
+++ app/src/main/java/ca/tetervak/kittymessage4/MainActivity.kt
@@ -3,9 +3,11 @@ package ca.tetervak.kittymessage4
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
+import androidx.navigation.Navigation
 import ca.tetervak.kittymessage4.model.Envelope
 import ca.tetervak.kittymessage4.ui.InputFragment
 import ca.tetervak.kittymessage4.ui.OutputFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),
     InputFragment.InputListener, OutputFragment.OutputListener {
@@ -17,23 +19,17 @@ class MainActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if(savedInstanceState == null){
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, InputFragment())
-                .commit()
-        }
     }
 
     override fun showOutput(envelope: Envelope) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, OutputFragment.newInstance(envelope))
-            .addToBackStack(INPUT_TO_OUTPUT)
-            .commit()
+        val arguments = Bundle()
+        arguments.putSerializable(OutputFragment.ENVELOPE, envelope)
+        Navigation.findNavController(this, R.id.nav_host)
+            .navigate(R.id.action_input_to_output, arguments)
     }
 
     override fun showInput(){
-        supportFragmentManager.popBackStack(
-            INPUT_TO_OUTPUT, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        Navigation.findNavController(this, R.id.nav_host)
+            .navigate(R.id.action_output_to_input)
     }
 }
