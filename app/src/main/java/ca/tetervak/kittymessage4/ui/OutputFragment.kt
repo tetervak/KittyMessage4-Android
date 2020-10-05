6e2f9118bf8ab2f71eb2b9d08aac3b59068c183f
Alex Tetervak <tetervak@sheridancollege.ca>
2020-10-05 10:50:47 -0400

    simplified the inflate logic

--- app/src/main/java/ca/tetervak/kittymessage4/ui/OutputFragment.kt
+++ app/src/main/java/ca/tetervak/kittymessage4/ui/OutputFragment.kt
@@ -30,8 +30,7 @@ class OutputFragment : Fragment() {
         }
     }
 
-    private var _binding: FragmentOutputBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentOutputBinding
 
     private var envelope: Envelope? = null
 
@@ -40,7 +39,7 @@ class OutputFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentOutputBinding.inflate(inflater, container, false)
+        binding = FragmentOutputBinding.inflate(inflater, container, false)
 
         binding.backButton.setOnClickListener { outputListener?.showInput() }
 
